a4a21e38a701a0ae23039bbff02c6d13be98c1a4
Danny Huang <dhuang50@stuy.edu>
2024-10-18 20:55:34 -0400

    Finished 19

--- 19_db0/build_db.py
+++ 19_db0/build_db.py
@@ -1,5 +1,4 @@
 
-
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
 
@@ -10,31 +9,50 @@ db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
 
 #==========================================================
-students = {}
-courses = {}
 
-with open('students.csv', newline='') as csvfile:
-    students = csv.DictReader(csvfile)
+studentcsv =  open('students.csv', newline='')
+students = csv.DictReader(studentcsv)
     
-with open('courses.csv', newline='') as csvfile:
-    courses = csv.DictReader(csvfile)
+coursescsv = open('courses.csv', newline='')
+courselist = csv.DictReader(coursescsv)
     
+courses= []
 diffcourses = []
-for i in courses:
-    if not(i["code"] in courses):
+for i in courselist:
+    courses.append(i)
+    if not(i["code"] in diffcourses):
         diffcourses.append(i["code"])
-        
+  
 studentlists = []
 for student in students:
     studentdata = {}
-    
-    
+    studentdata['name'] = student['name']
+    studentdata['id'] = student['id']
+    studentdata['age'] = student['age']
+    individual_course =[]
+    print(student['id'])
     for i in courses:
-        if(i["id"] = student["id"]):
+        print(i['id'])
+        if i['id'] == student['id']:
+            studentdata[f"{i['code']}"] = i['mark']
+            individual_course.append(i['code'])
             
+    for i in diffcourses:
+        if not i in individual_course:
+            studentdata[f'{i}'] = 'NULL'
+    studentlists.append(studentdata)
+print(studentlists)
 
+c.execute("DROP TABLE IF EXISTS roster")
+c.execute(f"CREATE TABLE roster(name TEXT, id INTEGER, age INTEGER, {diffcourses[0]}, {diffcourses[1]}, {diffcourses[2]}, {diffcourses[3]}, {diffcourses[4]})")
+c.execute("SELECT * FROM roster")
 
-command = ""          # test SQL stmt in sqlite3 shell, save as string
+for i in studentlists:
+    c.execute(f"INSERT INTO roster VALUES ('{i['name']}', {i['id']}, {i['age']}, {i[diffcourses[0]]}, {i[diffcourses[1]]}, {i[diffcourses[2]]}, {i[diffcourses[3]]}, {i[diffcourses[4]]})")
+
+    
+
+command = "SELECT * FROM roster"          # test SQL stmt in sqlite3 shell, save as string
 c.execute(command)    # run SQL statement
 
 #==========================================================
