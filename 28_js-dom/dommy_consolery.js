0b275f1821179472ce41af2d5f0eba84d5259073
Danny Huang <dhuang50@stuy.edu>
2025-01-07 20:45:59 -0500

    Updated 28:

--- 28_js-dom/dommy_consolery.js
+++ 28_js-dom/dommy_consolery.js
@@ -1,35 +1,5 @@
-/*
-  your PPTASK:
-  
-  First, familiarize yourself with the given html file for this work.
-
-      then...
-
-  Test drive each bit of code in this file,
-  and insert comments galore, indicating anything
-  you discover,
-  have questions about,
-  or otherwise deem notable.
-
-  Have the given html file open as you work.
-  
-  Write with your future self or teammates in mind.
-  
-  If you find yourself falling out of flow mode, consult 
-  - other teams
-  - MDN
-
-  A few comments have been pre-filled for you...
-  
-  (delete this block comment once you are done)
-*/
-
-
-
-
-
-// Team Phantom Tollbooth :: Clyde Sinclair, Fierce Dragon 
-// SoftDev pd0
+// Team JD :: Jayden Zhang, Danny Huang
+// SoftDev pd5
 // K28 -- Getting more comfortable with the dev console and the DOM
 // 2025-01-07t
 // --------------------------------------------------
@@ -39,14 +9,14 @@
 //(Ctrl-Shift-K in Firefox to reveal console)
 console.log("AYO");
 
-var i = "hello";
-var j = 20;
+var i = "hello"; // global variables
+var j = 20; 
 
 
 //assign an anonymous fxn to a var
 var f = function(x) 
 {
-    var j=30;
+    var j=30; // variable in a variable [local variable]
     return j+x;
 };
 
@@ -56,41 +26,41 @@ var o = { 'name' : 'Thluffy',
           age : 1024,
           items : [10, 20, 30, 40],
           morestuff : {a : 1, b : 'ayo'},
-          func : function(x) {
+          func : function(x) { // function in a object, can be called with o.func(x)
               return x+30;
           }
         };
 
 //create a new node in the tree
-var addItem = function(text)
+var addItem = function(text) // adds a bullet point to the list.
 {
-    var list = document.getElementById("thelist");
-    var newitem = document.createElement("li");
+    var list = document.getElementById("thelist"); // calls the list with the HTML ID
+    var newitem = document.createElement("li"); // creates next list item
     newitem.innerHTML = text;
-    list.appendChild(newitem);
+    list.appendChild(newitem); // add it to the list and HTML updates [adds LOCALLY, does not directly change HTML file]
 };
 
 //prune a node from the tree
-var removeItem = function(n)
+var removeItem = function(n) // removes a list item by the index
 {
-    var listitems = document.getElementsByTagName('li');
-    listitems[n].remove();
+    var listitems = document.getElementsByTagName('li'); // retrieves a list item.
+    listitems[n].remove(); // removes it LOCALLY from the list,
 };
 
 //color selected elements red
-var red = function()
+var red = function() // note: prioritizes blue color for some reason; works if you manually clear all class names.
 {
-    var items = document.getElementsByTagName("li");
+    var items = document.getElementsByTagName("li"); // retrieves list item.
     for(var i = 0; i < items.length; i++) {
-	items[i].classList.add('red');
+	items[i].classList.add('red'); // adds red to the class of each item in the list to turn them red using css.
     }
 };
 
 //color a collection in alternating colors
-var stripe = function()
+var stripe = function() // note: prioritizes blue color for some reason; works if you manually clear all class names.
 {
-    var items = document.getElementsByTagName("li");
-    for(var i = 0; i < items.length; i++) {
+    var items = document.getElementsByTagName("li"); // gets all list items on the document.
+    for(var i = 0; i < items.length; i++) { // changes them into alternating red and blue locally. 
 	if (i%2==0) {
 	    items[i].classList.add('red');
 	} else {
@@ -102,16 +72,41 @@ var stripe = function()
 
 //insert your implementations here for...
 // FIB
+let fib = function(n){
+    if(n == 0){
+        return 0;
+    }
+    else if (n == 1) {
+        return 1;
+    }
+    else {
+        return fib(n-1) + fib(n-2);
+    }
+}
 // FAC
+let fact = function(n){
+    if (n == 1){
+        return 1;
+    }
+    else{
+        return n*fact(n-1);
+    }
+}
 // GCD
-
+let gcd = function(a, b){
+    var gcd
+    for(i=0; i <= Math.min(a, b); i++){
+        if(a%i==0 && b%i==0){
+            gcd = i;
+        }
+    }
+    return gcd
+}
 
 // In addition to the style shown above,
 //  you are encouraged to test drive the "arrow function syntax" as shown below.
 //  Note anything notable.
-const myFxn = (param1, param2) => {
+const myFxn = (param1, param2) => {  // note: => is equailavent to function and is placed after the parameters
     // body
     return retVal;
 };
-
-
