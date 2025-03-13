c3c297d95d1eabb5fa545f4cd330a03d74eb4575
Danny Huang <144459270+Dhuang50@users.noreply.github.com>
2025-03-13 11:26:12 -0400

    Update routes.py

--- k35/app/routes.py
+++ k35/app/routes.py
@@ -1,56 +1,60 @@
-from flask import render_template, url_for, flash, redirect
-from k35.app import app, db, bcrypt
-from k35.app.forms import RegistrationForm, LoginForm, PostForm
-from k35.app.models import User, Post
-from flask_login import login_user, current_user, logout_user, login_required
+from flask import render_template, request, redirect, url_for, flash, session
+from k35.app import app
+import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
+from k35.app.models import query_db, execute_db
 
 @app.route("/")
 @app.route("/home")
 def home():
-    posts = Post.query.all()
+    posts = query_db("SELECT * FROM post")
     return render_template('index.html', posts=posts)
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
-    if current_user.is_authenticated:
+    if 'user_id' in session:
         return redirect(url_for('home'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(user)
-        db.session.commit()
-        flash('Your account has been created! You are now able to log in', 'success')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = generate_password_hash(request.form['password'])
+        try:
+            execute_db("INSERT INTO user (username, email, password) VALUES (?, ?, ?)", (username, email, password))
+            flash('Your account has been created! You are now able to log in', 'success')
+            return redirect(url_for('login'))
+        except sqlite3.IntegrityError:
+            flash('Username or email already exists', 'danger')
+    return render_template('register.html')
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated:
+    if 'user_id' in session:
         return redirect(url_for('home'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = query_db("SELECT * FROM user WHERE email = ?", (email,), one=True)
+        if user and check_password_hash(user['password'], password):
+            session['user_id'] = user['id']
+            flash('Login successful', 'success')
             return redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
+    return render_template('login.html')
 
 @app.route("/logout")
 def logout():
-    logout_user()
+    session.pop('user_id', None)
     return redirect(url_for('home'))
 
 @app.route("/post/new", methods=['GET', 'POST'])
-@login_required
 def new_post():
-    form = PostForm()
-    if form.validate_on_submit():
-        post = Post(title=form.title.data, content=form.content.data, author=current_user)
-        db.session.add(post)
-        db.session.commit()
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        execute_db("INSERT INTO post (title, content, user_id) VALUES (?, ?, ?)", (title, content, session['user_id']))
         flash('Your post has been created!', 'success')
         return redirect(url_for('home'))
-    return render_template('create_post.html', title='New Post', form=form)
+    return render_template('create_post.html')
